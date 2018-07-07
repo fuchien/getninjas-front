629d2ada48899085931919d3cc7911e78233b54b
Chiien <fuchienhsu@hotmail.com>
2018-07-07 17:28:30 -0300

    Update container style and request-fields script

--- js/form/form.js
+++ js/form/form.js
@@ -3,12 +3,15 @@ import renderRequestFields from './request-fields/request-fields';
 const createForm = (data) => {
     let form = (
         `
-            <div>${renderRequestFields(data._embedded.request_fields)}</div>
+            <div class="form__request">
+                ${renderRequestFields(data._embedded.request_fields)}
+            </div>
         `
     )
 }
 
 export default function renderForm(data, element) {
-    createForm(data)
     console.log(data);
+    const form = createForm(data);
+    element.innerHTML = form;
 }
--- js/form/request-fields/request-fields.js
+++ js/form/request-fields/request-fields.js
@@ -1,8 +1,9 @@
 
-export default function renderRequestFields(data) {
-    return (
-        `
-
-        `
-    )
+export default function renderRequestFields(requestFields) {
+    console.log(requestFields)
+    return requestFields.map(requestField => 
+        `<div>
+            <h4>${requestField.name}</h4>
+        </div>`
+    ).join('');
 }
