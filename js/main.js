c724b62a00484f3c804916561d88e2c7fc7919e8
Chiien <fuchienhsu@hotmail.com>
2018-07-07 16:51:45 -0300

    Import renderHeader in main.js and show

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 import renderForm from './form/form';
+import renderHeader from './header/header';
 
+const headerElement = document.getElementById('header');
 const formElement = document.getElementById('form');
 
 const getFieldsData = async (endpoint) => {
     try {
         const response = await fetch(endpoint);
         const data = await response.json();
+        renderHeader(headerElement);
         renderForm(data, formElement);
     } catch (err) {
         console.log(`Erro ao pegar os dados!`, err)
