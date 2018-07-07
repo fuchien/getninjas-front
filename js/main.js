c720d4f5af77ac7f5963ffabebf2595a723a16c1
Chiien <fuchienhsu@hotmail.com>
2018-07-07 13:00:39 -0300

    Add try catch function in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 
 
 const getFieldsData = async (endpoint) => {
-    const response = await fetch(endpoint);
-    const data = response.json()
-    console.log(data)
+    try {
+        const response = await fetch(endpoint);
+        const data = await response.json()
+        console.log(data)
+    } catch (err) {
+        console.log(`Erro ao pegar os dados!`, err)
+    }
 }
 
 getFieldsData(`http://localhost:3000/`);
