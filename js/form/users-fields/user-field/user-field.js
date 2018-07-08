e9789d489be187711e3fdbc3e6c3aa596d155b3a
Chiien <fuchienhsu@hotmail.com>
2018-07-08 12:52:10 -0300

    Add input type

--- js/form/users-fields/user-field/user-field.js
+++ js/form/users-fields/user-field/user-field.js
@@ -2,11 +2,16 @@
 
 export default function renderUserField(userField) {
 
+    let inputType = userField.type.toLowerCase() === 'email' ? 'email' :
+                    userField.type.toLowerCase() === 'cep' ? 'number' :
+                    userField.type.toLowerCase() === 'phone' ? 'number' :
+                    'text'
+
     return (
         `
         <div class="form__group">
             <label for="${userField.name}">${userField.label}</label>
-            <input type="text" id="${userField.name}" class="form__input" placeholder="${userField.placeholder}" required>
+            <input type="${inputType}" id="${userField.name}" class="form__input" placeholder="${userField.placeholder}" required>
         </div>
         `
     )
