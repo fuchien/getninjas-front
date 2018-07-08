f55b32561a5714a57319b936542431f4c787273a
Chiien <fuchienhsu@hotmail.com>
2018-07-08 12:40:57 -0300

    Update forms style

--- js/form/users-fields/user-field/user-field.js
+++ js/form/users-fields/user-field/user-field.js
@@ -4,9 +4,9 @@ export default function renderUserField(userField) {
 
     return (
         `
-        <div class="form-group">
+        <div class="form__group">
             <label for="${userField.name}">${userField.label}</label>
-            <input type="text" id="${userField.name}" placeholder="${userField.placeholder}" required>
+            <input type="text" id="${userField.name}" class="form__input" placeholder="${userField.placeholder}" required>
         </div>
         `
     )
