8546c59a5681421c677ba82bdb78fbeb51132a22
Chiien <fuchienhsu@hotmail.com>
2018-07-09 00:26:20 -0300

    Add mask to CEP

--- js/form/form-mask/form-mask.js
+++ js/form/form-mask/form-mask.js
@@ -1,5 +1,16 @@
 
 
 export function addCefMask(document) {
-    
+    let valor = document.replace(/\D/g, '');
+    if (valor.length == 0) {
+        valor = '';
+    } else if (valor.length <= 5) {
+        valor = valor.replace(/^(\d{0,5})/, '$1');
+    } else if (valor.length <= 6) {
+        valor = valor.replace(/^(\d{0,5})(\d{0,1})/, '$1-$2');
+    } else {
+        valor = valor.substr(0, 8).replace(/^(\d{0,5})(\d{0,3})/, '$1-$2');
+    }
+    console.log(valor)
+    return valor
 }
--- js/form/users-fields/user-field/user-field.js
+++ js/form/users-fields/user-field/user-field.js
@@ -1,17 +1,27 @@
 import renderRequiredField from '../../required-field/required-field';
+import { addCefMask } from '../../form-mask/form-mask';
 
 export default function renderUserField(user) {
 
     let inputType = user.userField.type.toLowerCase() === 'email' ? 'email' :
-                    user.userField.type.toLowerCase() === 'cep' ? 'number' :
+                    user.userField.type.toLowerCase() === 'cep' ? 'text' :
                     user.userField.type.toLowerCase() === 'phone' ? 'number' :
                     'text';
+
+    const changeValue = (value, id) => {
+        if (id.toLowerCase() === 'cep') {
+            let valueMasked = addCefMask(value);
+            document.getElementById(id).value = valueMasked;
+        }
+    }
+    
+    window.changeValue = changeValue;
 
     return (
         `
         <div class="form__group">
             <label for="${user.userField.name}">${user.userField.label}</label>
-            <input type="${inputType}" name="${user.userField.name}" id="${user.userField.name}"style="width: ${user.index === 0 || user.index === 3 ? '40%' : '100%'}"
+            <input type="${inputType}" onkeyup="changeValue(this.value, this.id)" name="${user.userField.name}" id="${user.userField.name}"style="width: ${user.index === 0 || user.index === 3 ? '40%' : '100%'}"
                 class="form__input" placeholder="${user.userField.placeholder}" ${user.userField.required ? 'required' : ''}>
             ${user.userField.required ? renderRequiredField() : ''}
         </div>
